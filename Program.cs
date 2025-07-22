bdbe9fa2c19e12473d43b503cd486ac2446a0bd8
drp_ <mve45@mail.ru>
2025-07-22 18:38:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите номер задачи (или запустите тесты): ");
+        if (!int.TryParse(Console.ReadLine(), out int taskNumber))
+        {
+            Console.WriteLine("Некорректный ввод. Запускаем тесты.");
+            return;
+        }
+
+
+        switch (taskNumber)
+        {
+
+
+            default:
+                Console.WriteLine("=== Запуск всех тестов ===");
+                break;
+        }
+    }
+
+
+}
